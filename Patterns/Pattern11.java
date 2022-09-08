5d4ac7f87da8b6fa191fe477847e8c8216614a9d
jisu2000 <spjisu2000@gmail.com>
2022-09-08 14:41:59 +0530

    //     * * * * *     //   * * * *     //     * * *     //       * *     //         *

---
+++ Patterns/Pattern11.java
@@ -0,0 +1,26 @@
+public class Pattern11 {
+
+
+    // * * * * * 
+    //   * * * * 
+    //     * * * 
+    //       * * 
+    //         * 
+
+    static void print(int n) {
+        for (int row = 1; row <= n; row++) {
+            int space = (n + row - 1) - n;
+            for (int i = 1; i <= space; i++) {
+                System.out.print("  ");
+            }
+            for (int col = 1; col <= (n - row) + 1; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+    public static void main(String[] args) {
+        print(5);
+    }
+}
