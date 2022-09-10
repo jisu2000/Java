fffd12af22b46af46831314dd0b2a96ccff24156
jisu2000 <spjisu2000@gmail.com>
2022-09-10 07:24:47 +0530

    Ceiling element added

---
+++ Searching/Ceiling.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+//ceiling element is the element which == of smallest greater than the target element//
+
+public class Ceiling {
+
+    public static int Search(int[] arr, int target) {
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            else if (target < arr[mid]) {
+                end = mid - 1;
+            }
+
+            else if (target > arr[mid]) {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter the length of the Array");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter " + n + " elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("The Array You have Entered is");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Enter the Element you want to find");
+        int target = sc.nextInt();
+
+        int ans = Search(arr, target);
+
+        System.out.println(target + " should be in " + ans + " position");
+
+    }
+
+}
