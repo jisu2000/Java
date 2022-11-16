a1b79e4cef454ed177151d17ca4a33d5032b088b
jisu2000 <spjisu2000@gmail.com>
2022-11-16 23:31:22 +0530

    No need of Stack

--- String/LengthOfLast.java
+++ String/LengthOfLast.java
@@ -1,29 +1,28 @@
-import java.util.Stack;
+
 
 public class LengthOfLast {
-    public static  int lengthOfLastWord(String s) {
-        
-        Stack<Character> stack=new Stack<>();
+    public static int lengthOfLastWord(String s) {
 
-        boolean first=false;
-        for(int i=s.length()-1;i>=0;i--)
-        {
-            if(Character.isLetter(s.charAt(i)))
-            {
-                stack.push(s.charAt(i));
-                first=true;
+        boolean first = false;
+        int c = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (Character.isLetter(s.charAt(i))) {
+
+                first = true;
+                c++;
+
             }
 
-            if(first && s.charAt(i)==' ')
-            {
+            if (first && s.charAt(i) == ' ') {
                 break;
             }
         }
-        return stack.size();
+        return c;
     }
+
     public static void main(String[] args) {
-        String s="   fly me   to   the moon  ";
-        int ans=lengthOfLastWord(s);
+        String s = "   fly me   to   the moon  ";
+        int ans = lengthOfLastWord(s);
         System.out.println(ans);
     }
 }
